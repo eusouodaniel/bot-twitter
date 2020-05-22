4864431fe0096e3840f2a59b3b1ceaa4def5dda6
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-05-21 21:56:24 -0300

    NEW: Refac with integration TT

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-const Twit = require('twit');
 require('dotenv').config();
-
-const Bot = new Twit({
-  consumer_key:         process.env.CONSUMER_KEY,
-  consumer_secret:      process.env.CONSUMER_SECRET,
-  access_token:         process.env.ACCESS_TOKEN,
-  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms:           60 * 1000,
-});
+const Bot = require('./api-twitter');
 
 function BotInit() {
 	var query = {
-		q: "#TodosPeloImpeachment",
-		result_type: "recent"
+		q: process.env.HASHTAG_TT,
+		result_type: process.env.HASHTAG_TYPE
 	}
 
-	Bot.get('search/tweets', query, BotGotLatestTweet);
+	Bot.get(process.env.API_SEARCH_TWEETS, query, BotGotLatestTweet);
 
 	function BotGotLatestTweet(error, data) {
 		if (error) {
-			console.log('Error: ' + error);
+			console.log('Error in search: ' + error);
 		} else {
 			var id = {
-				id : data.statuses[0].id_str
+				id: data.statuses[0].id_str
 			}
 
-			Bot.post('statuses/retweet/:id', id, BotRetweeted);
+			Bot.post(`${process.env.API_RETWEET}/:id`, id, BotRetweeted);
 			
 			function BotRetweeted(error) {
 				if (error) {
